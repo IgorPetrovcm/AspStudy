2ac6d4189fdfae561070f65421b7b11d608c74c7
igoradm <igorpetrov42ru@gmail.com>
2023-11-12 19:07:49 +0300

    IActionResult реализует также ответ в виде json файла, спокойно отправляет сложные объекты и массивы этих объектов

--- Task_11/Controllers/Home.cs
+++ Task_11/Controllers/Home.cs
@@ -7,5 +7,17 @@ namespace MvcApp
         {
             return new HtmlResult("<h1>HELLO WORLD!!!</h1>");
         }
+        public IActionResult Index2() 
+        {
+            return Content("Hello World!");
+        }
+        //JsonResult
+        public IActionResult Index3() 
+        {
+            Person person = new Person("Tom",22);
+            Person[] persons = {person};
+            return Json(persons); // json также возвращает и массивы
+        }
     }
+    public record class Person(string name, int age);
 }
